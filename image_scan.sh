f91ba888ae674fe33361ae4ab3d3c21d29a10aae
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2023-08-15 19:42:17 +0530

    Create image_scan.sh

---
+++ image_scan.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+repo_name=demo
+image_tag=mahesh-16
+arn=arn:aws:sns:ap-south-1:351836203514:sample
+critical_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag | grep -i "findingSeverityCounts" -A 5 | grep -i critical | cut -d ":" -f 2 | tr -d ",")
+high_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag | grep -i "findingSeverityCounts" -A 5 | grep -i high | cut -d ":" -f 2 | tr -d ",")
+
+if [ -z "$critical_vulnr" ]; then
+    critical_vulnr=0
+fi
+
+if [ -z "$high_vulnr" ]; then
+    high_vulnr=0
+fi
+
+echo "High vulnerabilities: $high_vulnr"
+echo "Critical vulnerabilities: $critical_vulnr"
+
+if [[ $high_vulnr -gt 0 && $critical_vulnr -ge 0  ]]
+then
+    echo "Your image has high vulnerabilities" && aws sns publish --topic-arn $arn --message "Image vulnerabilities: High vulnerabilities detected for image $image_tag in repository $repo_name" --subject "Image Vulnerability Alert"
+
+    exit 1 
+else
+    echo "Your image has low vulnerabilities"
+fi
