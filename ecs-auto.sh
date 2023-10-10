a9a66376a58afcff828bc085bd909a98d5c61ed6
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2023-10-10 17:46:23 +0530

    Update ecs-auto.sh

--- ecs-auto.sh
+++ ecs-auto.sh
@@ -5,8 +5,8 @@ AWS_REGION="ap-south-1"
 export AWS_PROFILE=default
 
 # Register a new Task definition 
-aws ecs register-task-definition --family anuj-new --cli-input-json file://task-new.json --region $AWS_REGION
+aws ecs register-task-definition --family demo-new --cli-input-json file://task-new.json --region $AWS_REGION
 
 # Update Service in the Cluster
-aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE --task-definition anuj-new --desired-count 1 --region $AWS_REGION
+aws ecs update-service --cluster $CLUSTER_NAME --service $SERVICE --task-definition demo-new --desired-count 1 --region $AWS_REGION
 
