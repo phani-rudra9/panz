55d796dcd021c2405a0693c7505059206acc975c
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2023-10-10 17:36:56 +0530

    Update image_scan.sh

--- image_scan.sh
+++ image_scan.sh
@@ -1,26 +1,23 @@
 #!/bin/bash
-repo_name=demo
-image_tag=SAMPLE-PROJECT-${BUILD_NUMBER}
-arn=arn:aws:sns:ap-south-1:351836203514:sample
-critical_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag | grep -i "findingSeverityCounts" -A 5 | grep -i critical | cut -d ":" -f 2 | tr -d ",")
-high_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag | grep -i "findingSeverityCounts" -A 5 | grep -i high | cut -d ":" -f 2 | tr -d ",")
+repo_name="demo"
+image_tag="SAMPLE-PROJECT-${BUILD_NUMBER}"
+arn="arn:aws:sns:ap-south-1:351836203514:sample"
 
-if [ -z "$critical_vulnr" ]; then
-    critical_vulnr=0
-fi
+# Get image scan findings as JSON
+image_scan_findings=$(aws ecr describe-image-scan-findings --repository-name "$repo_name" --image-id imageTag="$image_tag")
 
-if [ -z "$high_vulnr" ]; then
-    high_vulnr=0
-fi
+# Extract critical and high vulnerability counts using jq
+critical_vulnr=$(echo "$image_scan_findings" | jq '.imageScanFindings.findingSeverityCounts."CRITICAL" // 0')
+high_vulnr=$(echo "$image_scan_findings" | jq '.imageScanFindings.findingSeverityCounts."HIGH" // 0')
 
 echo "High vulnerabilities: $high_vulnr"
 echo "Critical vulnerabilities: $critical_vulnr"
 
-if [[ $high_vulnr -gt 0 && $critical_vulnr -ge 0  ]]
+if [[ $high_vulnr -gt 0 || $critical_vulnr -gt 0 ]]
 then
-    echo "Your image has high vulnerabilities" && aws sns publish --topic-arn $arn --message "Image vulnerabilities: High vulnerabilities detected for image $image_tag in repository $repo_name" --subject "Image Vulnerability Alert"
-
-    exit 1 
+    echo "Your image has high or critical vulnerabilities"
+    aws sns publish --topic-arn "$arn" --message "Image vulnerabilities: High or critical vulnerabilities detected for image $image_tag in repository $repo_name" --subject "Image Vulnerability Alert"
+    exit 1
 else
-    echo "Your image has low vulnerabilities"
+    echo "Your image has no high or critical vulnerabilities"
 fi
