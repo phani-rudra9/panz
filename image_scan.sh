b1f8c70dccfd7f41dd693a5e975ca78a7784edb6
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2023-12-11 15:19:54 +0530

    Update image_scan.sh

--- image_scan.sh
+++ image_scan.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 repo_name=demo
-image_tag=SAMPLE-PROJECT-${BUILD_NUMBER}
+image_tag=dotnet-app-${BUILD_NUMBER}
 arn=arn:aws:sns:ap-south-1:351836203514:sample
 critical_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag | grep -i "findingSeverityCounts" -A 5 | grep -i critical | cut -d ":" -f 2 | tr -d ",")
 high_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag | grep -i "findingSeverityCounts" -A 5 | grep -i high | cut -d ":" -f 2 | tr -d ",")
@@ -18,8 +18,8 @@ echo "Critical vulnerabilities: $critical_vulnr"
 
 if [[ $high_vulnr -gt 0 && $critical_vulnr -ge 0  ]]
 then
-    echo "Your image has high vulnerabilities" && aws sns publish --topic-arn $arn --message "Image vulnerabilities: High vulnerabilities detected for image $image_tag in repository $repo_name" --subject "Image Vulnerability Alert"
-
+    echo "Your image has high vulnerabilities"
+    # aws sns publish --topic-arn $arn --message "Image vulnerabilities: High vulnerabilities detected for image $image_tag in repository $repo_name" --subject "Image Vulnerability Alert"
     exit 1 
 else
     echo "Your image has low vulnerabilities"
