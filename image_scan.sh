3e9228fe4bc88aa4758a36df4ccbc621c3f29f9a
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2023-12-19 11:20:06 +0530

    Update image_scan.sh

--- image_scan.sh
+++ image_scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 repo_name=demo
 image_tag=demo-project-${BUILD_NUMBER}
-# arn=arn:aws:sns:us-east-2:971076122335:demo
+arn=arn:aws:sns:us-east-2:971076122335:demo
 critical_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag --region us-east-2 | grep -i "findingSeverityCounts" -A 5 | grep -i critical | cut -d ":" -f 2 | tr -d ",")
 high_vulnr=$(aws ecr describe-image-scan-findings --repository-name $repo_name --image-id imageTag=$image_tag --region us-east-2 | grep -i "findingSeverityCounts" -A 5 | grep -i high | cut -d ":" -f 2 | tr -d ",")
 
@@ -18,8 +18,8 @@ echo "Critical vulnerabilities: $critical_vulnr"
 
 if [[ $high_vulnr -gt 0 || $critical_vulnr -gt 0 ]]; then
    echo "Your image is having vulnerabilities, please check...."
-   #aws sns publish --topic-arn $arn --message "Your image in this $repo_name repo with image tag $image_tag has vulnerabilities, please check..."
-   #exit 1
+   aws sns publish --topic-arn $arn --message "Your image in this $repo_name repo with image tag $image_tag has vulnerabilities, please check..."
+   exit 1
 else
     echo "Your image is safe for deployment"
 fi
