ab6883a57d19d71c3695366e852252232850402f
phani-rudra9 <53288879+phani-rudra9@users.noreply.github.com>
2023-12-19 13:15:32 +0530

    Update sonar_scan.sh

--- sonar_scan.sh
+++ sonar_scan.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-curl -u admin:phani  http://18.144.62.206:9000/api/qualitygates/project_status?projectKey=demo > demo.json
+curl -u admin:rudra  http://54.67.17.31:9000/api/qualitygates/project_status?projectKey=demo > demo.json
 sonar_scan=$(cat demo.json | grep -i status | cut -d ':' -f 3 | cut -d ',' -f 1 | tr -d '"')
-if [[ sonar_scan -eq ok ]]
+if [[ $sonar_scan -eq ok ]]
 then
     echo "sonar_scan_status is $sonar_scan and it's safe"
 else
